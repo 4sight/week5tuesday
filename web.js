cff26a54d046769d0cd631183498dbd8c4cfe84d
Greeley Dawson <hgd4th@gmail.com>
2014-07-01 23:40:55 -0400

    Added Procfile

--- web.js
+++ web.js
@@ -1,1 +1,6 @@
-web: node web.js
+var gzippo = require('gzippo');
+    var express = require('express');
+    var app = express();
+    
+    app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+    app.listen(process.env.PORT || 5000);
